026837fb5b16bdffd8cc329877bbcda88bbc0e77
hossainchisty <hossain.chisty11@gmail.com>
2021-04-02 21:04:25 +0600

    add CURD oprations

---
+++ Project/Todo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import  path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('update_task/<str:pk>', views.update_tasks, name="update"),
+     path('delete_task/<str:pk>/', views.delete_tasks, name="delete_task")
+]
