6f6bfbb1b73e9db572a41367f5abd748dea8de29
hossainchisty <hossain.chisty11@gmail.com>
2021-04-03 20:17:08 +0600

    remove whitespace

--- Project/Todo/urls.py
+++ Project/Todo/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('update_task/<str:pk>', views.update_tasks, name="update"),
-     path('delete_task/<str:pk>/', views.delete_tasks, name="delete_task")
+    path('delete_task/<str:pk>/', views.delete_tasks, name="delete_task")
 ]
